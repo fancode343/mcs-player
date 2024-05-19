4faed24c3247f22ec8fc17804a12bf92f1f58a88
fancode343 <118608942+fancode343@users.noreply.github.com>
2024-05-19 15:18:10 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -29,6 +29,5 @@ echo "Redirecting to Register";
 "sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
 		crossorigin="anonymous">
 </head>
-<script>window.location.replace("register");
-    </script>
+
 </html>
