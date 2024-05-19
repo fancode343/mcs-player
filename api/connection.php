c861daad519e53a09fd50d6bf164b69139d89246
fancode343 <118608942+fancode343@users.noreply.github.com>
2024-05-19 20:50:05 +0800

    Update connection.php

--- api/connection.php
+++ api/connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbhost = "sql.freedb.tech";
-$dbuser = "freedb_Gran5";
-$dbpass = "7#EKrSExgayC7@h";
-$dbname = "freedb_LoginGuormit";
+$dbhost = "Dsbot-database-465e21d6d5.onlitegix.com";
+$dbuser = "GGMC";
+$dbpass = "thelegend";
+$dbname = "";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 {
