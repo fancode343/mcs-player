93851a56aafe8dc600cf906b80169f7ea3587f2f
fancode343 <118608942+fancode343@users.noreply.github.com>
2024-05-20 20:06:54 +0800

    Create create_database.php

---
+++ api/create_database.php
@@ -0,0 +1,29 @@
+<?php
+// Database credentials
+include("/var/task/user/api/connection.php");
+
+// SQL query to create table
+$sql = "CREATE TABLE Users (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(20) PRIMARY KEY,
+    username VARCHAR(50),
+    email VARCHAR(100),
+    age INT,
+    gender VARCHAR(10),
+    wdywtj TEXT,
+    phoneCode VARCHAR(15),
+    fbprof VARCHAR(255),
+    dsnm VARCHAR(255)
+
+)";
+
+// Execute query and check for errors
+if ($conn->query($sql) === TRUE) {
+    echo "Table Users created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+// Close the connection
+$conn->close();
+?>
