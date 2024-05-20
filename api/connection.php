9393f00fa748dad0429a770e8129bbd3a65e2e9a
fancode343 <118608942+fancode343@users.noreply.github.com>
2024-05-20 19:53:45 +0800

    Update connection.php

--- api/connection.php
+++ api/connection.php
@@ -2,7 +2,7 @@
 // Enable detailed error reporting
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$dbhost = "d0b.h.filess.io";
+$dbhost = "d0b.h.filess.io:3307";
 $dbuser = "ggmc321_expressmap";
 $dbpass = "13119e57f3ad90c4efd6b1906fbf150a74b1a388";
 $dbname = "ggmc321_expressmap";
