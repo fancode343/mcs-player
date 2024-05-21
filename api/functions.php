87995d6b15e029c8d8eae34693f057e778d0b22c
fancode343 <118608942+fancode343@users.noreply.github.com>
2024-05-21 14:10:44 +0800

    Update functions.php

--- api/functions.php
+++ api/functions.php
@@ -7,7 +7,7 @@ function check_login($con)
 	{
 
 		$id = $_SESSION['user_id'];
-		$query = "select * from MCDISC where user_id = '$id' limit 1";
+		$query = "select * from Users where user_id = '$id' limit 1";
 
 		$result = mysqli_query($con,$query);
 		if($result && mysqli_num_rows($result) > 0)
