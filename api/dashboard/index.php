03a9214ba01409ae4ebfd37ba7398cff43d9f805
fancode343 <118608942+fancode343@users.noreply.github.com>
2024-05-21 17:33:40 +0800

    Update index.php

--- api/dashboard/index.php
+++ api/dashboard/index.php
@@ -1,5 +1,12 @@
 <?php 
 session_start();
+
+if (!isset($_SESSION['user_id'])) {
+  // Redirect to login if not logged in
+  header("Location: login");
+  exit;
+}
+
 	include("/var/task/user/api/connection.php");
 	include("/var/task/user/api/functions.php");
 
